1b692cd21e3b574c65a43b7dcb0d9822f0c6ec88
nicol <nicol@LAPTOP-4CN8F2PR>
2020-08-06 10:01:13 +0200

    test badge

--- src/main/java/com/spendesk/architecture/messagerelay/MessageRelayApplication.java
+++ src/main/java/com/spendesk/architecture/messagerelay/MessageRelayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplicationx
 @EnableScheduling
 public class MessageRelayApplication {
 
