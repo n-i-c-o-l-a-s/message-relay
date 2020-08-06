fdd17d6b3edc3dbcf2fa4fedfd6aeb864cc3996e
nicol <nicol@LAPTOP-4CN8F2PR>
2020-08-06 10:03:00 +0200

    fix

--- src/main/java/com/spendesk/architecture/messagerelay/MessageRelayApplication.java
+++ src/main/java/com/spendesk/architecture/messagerelay/MessageRelayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplicationx
+@SpringBootApplication
 @EnableScheduling
 public class MessageRelayApplication {
 
